cf490a3858ec8c94dd0bf2db961de5d317e8e6ad
Ioannis Anagnostopoulos <johnanagno@localhost>
2017-08-14 05:01:54 +0100

    Cleanup

--- EvoAlgoNSS/helper_functions.h
+++ EvoAlgoNSS/helper_functions.h
@@ -15,32 +15,3 @@ std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vector)
 	}
 	return stream;
 }
-
-
-
-/*
-template<typename T>
-template<typename F, typename C>
-bool initial_solution_checker(F f, C c)
-{
-	if (tol > std::abs(fitness_cost))
-	{
-		T timer = 0;
-		display_results();
-		std::cout << "Elapsed time in seconds: " << timer << "\n";
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
-template<typename T, typename S>
-void Solver_base<T, S>::display_results()
-{
-	std::cout << "Optimum solution: " << min_cost << " Fitness Value: " << fitness_cost << "\n";
-	std::cout << "Population: " << individuals.size() << " Solved at iteration: " << iter << "\n";
-
-}
-*/
